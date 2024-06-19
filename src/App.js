1c1115bc91449c83eb4d727c9d74b9c3c4fc180a
PeterShin23 <pss9fp@virginia.edu>
2024-06-19 17:04:38 -0400

    checking deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Version 2</p>
+        <p>Version 2 updated</p>
       </header>
     </div>
   );
